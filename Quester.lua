bf204f1e7e73a69eff3b2e72d0344b7017a28cfc
Hendrik Leppkes <h.leppkes@gmail.com>
2014-09-20 14:55:19 +0200

    Add "spell" objective type

--- Quester.lua
+++ Quester.lua
@@ -343,7 +343,7 @@ function Quester:QUEST_LOG_UPDATE()
 					elseif objType == "reputation" then
 						itemDesc, numItems, numNeeded = MatchFaction(objDesc)
 						numItems, numNeeded = factionLabels[numItems], factionLabels[numNeeded]
-					elseif objType == "event" or objType == "log" then
+					elseif objType == "event" or objType == "log" or objType == "spell" then
 						itemDesc, numNeeded, numItems = objDesc, 1, (objComplete and 1 or 0)
 					else
 						print("Unknown quest objective type: " .. objType .. ", on quest: " .. title .. ", objective: " .. objDesc)
