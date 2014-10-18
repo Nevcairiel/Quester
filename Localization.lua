9480d3a5d01cba6517141a803e6ebb2756f698a7
Hendrik Leppkes <h.leppkes@gmail.com>
2014-10-18 17:07:57 +0200

    Fix copy pasta error in localization

--- Localization.lua
+++ Localization.lua
@@ -10,52 +10,52 @@ debug = true
 local L = LibStub("AceLocale-3.0"):NewLocale("Quester", "enUS", true, debug)
 --@localization(locale="enUS", format="lua_additive_table", same-key-is-true=true)@
 
-L = LibStub("AceLocale-3.0"):NewLocale("Mapster", "deDE")
+L = LibStub("AceLocale-3.0"):NewLocale("Quester", "deDE")
 if L then
 --@localization(locale="deDE", format="lua_additive_table", handle-unlocalized="ignore")@
 end
 
-L = LibStub("AceLocale-3.0"):NewLocale("Mapster", "esES")
+L = LibStub("AceLocale-3.0"):NewLocale("Quester", "esES")
 if L then
 --@localization(locale="esES", format="lua_additive_table", handle-unlocalized="ignore")@
 end
 
-L = LibStub("AceLocale-3.0"):NewLocale("Mapster", "esMX")
+L = LibStub("AceLocale-3.0"):NewLocale("Quester", "esMX")
 if L then
 --@localization(locale="esMX", format="lua_additive_table", handle-unlocalized="ignore")@
 end
 
-L = LibStub("AceLocale-3.0"):NewLocale("Mapster", "frFR")
+L = LibStub("AceLocale-3.0"):NewLocale("Quester", "frFR")
 if L then
 --@localization(locale="frFR", format="lua_additive_table", handle-unlocalized="ignore")@
 end
 
-L = LibStub("AceLocale-3.0"):NewLocale("Mapster", "itIT")
+L = LibStub("AceLocale-3.0"):NewLocale("Quester", "itIT")
 if L then
 --@localization(locale="itIT", format="lua_additive_table", handle-unlocalized="ignore")@
 end
 
-L = LibStub("AceLocale-3.0"):NewLocale("Mapster", "koKR")
+L = LibStub("AceLocale-3.0"):NewLocale("Quester", "koKR")
 if L then
 --@localization(locale="koKR", format="lua_additive_table", handle-unlocalized="ignore")@
 end
 
-L = LibStub("AceLocale-3.0"):NewLocale("Mapster", "ptBR")
+L = LibStub("AceLocale-3.0"):NewLocale("Quester", "ptBR")
 if L then
 --@localization(locale="ptBR", format="lua_additive_table", handle-unlocalized="ignore")@
 end
 
-L = LibStub("AceLocale-3.0"):NewLocale("Mapster", "ruRU")
+L = LibStub("AceLocale-3.0"):NewLocale("Quester", "ruRU")
 if L then
 --@localization(locale="ruRU", format="lua_additive_table", handle-unlocalized="ignore")@
 end
 
-L = LibStub("AceLocale-3.0"):NewLocale("Mapster", "zhCN")
+L = LibStub("AceLocale-3.0"):NewLocale("Quester", "zhCN")
 if L then
 --@localization(locale="zhCN", format="lua_additive_table", handle-unlocalized="ignore")@
 end
 
-L = LibStub("AceLocale-3.0"):NewLocale("Mapster", "zhTW")
+L = LibStub("AceLocale-3.0"):NewLocale("Quester", "zhTW")
 if L then
 --@localization(locale="zhTW", format="lua_additive_table", handle-unlocalized="ignore")@
 end
